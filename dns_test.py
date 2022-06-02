3281daa9c4bb105a60faffef88735856d153fff8
YMaster <helium747@gmail.com>
2022-06-02 12:43:21 +0800

    improve dns_test
    - fix random.seed problem
    - add shell script

--- dns_test.py
+++ dns_test.py
@@ -24,19 +24,21 @@ def domain_test(dns_server, domain):
     return res
 
 
-def random_domain_test(dns_server):
-    random_q = ''.join(random.choices(string.ascii_lowercase, k=5)) + '.com'
-    return domain_test(dns_server, random_q)
+def get_random_domain_list(n):
+    l = [''.join(random.choices(string.ascii_lowercase, k=5)) + '.com'
+            for _ in range(n)]
+    return l
 
 
 def print_counter(counter):
-    for k, v in counter.items():
+    l = list(counter.items())
+    l.sort()
+    for k, v in l:
         print(f'\t{k}: {v}')
 
 
 if __name__ == '__main__':
-    random.seed(42)
-    
+    random.seed(42) 
     LOCALHOST = '127.0.0.1'
     LOCAL_DNS = '192.168.2.1'
 
@@ -52,11 +54,8 @@ if __name__ == '__main__':
 
     print('----------Basic Test----------')
     n = 10
-    l = []
-    for i in range(n):
-        res = random_domain_test(srv)
-        l.append(res)
-    print(*l, sep='\n')
+    l = map(domain_test, [srv]*n, get_random_domain_list(n))
+    print(*l, sep='\n') 
     st_l = [x[1] for x in l]
     print('SUMMARY:', f'TOTAL {n}')
     print_counter(Counter(st_l))
@@ -66,8 +65,8 @@ if __name__ == '__main__':
     with mp.Pool(processes=100) as pool:
         print('----------50x Test----------')
         n = 50
-        l = pool.map(random_domain_test, [srv]*n)
-        print(*l, sep='\n')
+        l = pool.starmap(domain_test, zip([srv]*n, get_random_domain_list(n)))
+        print(*l, sep='\n') 
         st_l = [x[1] for x in l]
         print('SUMMARY:', f'TOTAL {n}')
         print_counter(Counter(st_l))
@@ -76,8 +75,8 @@ if __name__ == '__main__':
 
         print('----------200x Test----------')
         n = 200
-        l = pool.map(random_domain_test, [srv]*n)
-        # print(*l, sep='\n')
+        l = pool.starmap(domain_test, zip([srv]*n, get_random_domain_list(n)))
+        # print(*l, sep='\n') 
         st_l = [x[1] for x in l]
         print('SUMMARY:', f'TOTAL {n}')
         print_counter(Counter(st_l))
@@ -86,8 +85,8 @@ if __name__ == '__main__':
 
         print('----------500x Test----------')
         n = 500
-        l = pool.map(random_domain_test, [srv]*n)
-        # print(*l, sep='\n')
+        l = pool.starmap(domain_test, zip([srv]*n, get_random_domain_list(n)))
+        # print(*l, sep='\n') 
         st_l = [x[1] for x in l]
         print('SUMMARY:', f'TOTAL {n}')
         print_counter(Counter(st_l))
@@ -97,9 +96,10 @@ if __name__ == '__main__':
         print('----------1000x Test, sleep 1s per 100x----------')
         n = 100
         for i in range(10):
-            l += pool.map(random_domain_test, [srv]*n)
+            l += pool.starmap(domain_test,
+                    zip([srv]*n, get_random_domain_list(n)))
             sleep(1)
-        # print(*l, sep='\n')
+        # print(*l, sep='\n') 
         st_l = [x[1] for x in l]
         print('SUMMARY:', f'TOTAL {n*10}')
         print_counter(Counter(st_l))
---
+++ dns_test_compare.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python3 dns_test.py l >output_127001.txt
+python3 dns_test.py 8.8.8.8 >output_8888.txt
